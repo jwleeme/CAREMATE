66fe947c42cc5bc05077685cd83d708883eca4fb
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-11-21 00:18:34 +0900

    Refactor: validation

--- src/pages/user/register/authInfo/AuthInfo.jsx
+++ src/pages/user/register/authInfo/AuthInfo.jsx
@@ -16,11 +16,8 @@ export default function AuthInfo() {
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
 
-  //오류 메시지
-  const [emailStatus, setEmailStatus] = useState(InputStatus.NORMAL);
-  const [emailCodeStatus, setEmailCodeStatus] = useState(InputStatus.NORMAL);
-  const [passwordStatus, setPasswordStatus] = useState(InputStatus.NORMAL);
-  const [passwordConfirmStatus, setPasswordConfirmStatus] = useState(InputStatus.NORMAL);
+  // error 존재 여부 (email, emailCode, password, passwordConfirm)
+  const [hasError, setHasError] = useState([false, false, false, false]);
 
   const [isEmailVerified, setEmailVerified] = useState(false);
 
@@ -30,23 +27,18 @@ export default function AuthInfo() {
     setEmailVerified(true);
   };
 
-  const handleInputChange = (inputValue, inputName, setStatus, setState, password = null) => {
+  const handleInputChange = (inputValue, inputName, index, setState, password = null) => {
     // 유효성 검사 수행
     const status = validateInput(inputValue, inputName, password);
-    setStatus(status);
     setState(inputValue);
+    const newErrors = [...hasError];
+    newErrors[index] = status === InputStatus.ERROR;
+    setHasError(newErrors);
   };
 
   // 모든 필드가 유효하고 값이 존재하는지 확인
   const isValid =
-    emailStatus !== InputStatus.ERROR &&
-    emailCodeStatus !== InputStatus.ERROR &&
-    passwordStatus !== InputStatus.ERROR &&
-    passwordConfirmStatus !== InputStatus.ERROR &&
-    email !== '' &&
-    emailCode !== '' &&
-    password !== '' &&
-    passwordConfirm !== '';
+    !hasError.includes(true) && email !== '' && emailCode !== '' && password !== '' && passwordConfirm !== '';
 
   return (
     <div className={cx('wrapper')}>
@@ -54,52 +46,42 @@ export default function AuthInfo() {
       <div className={cx('registerContainer')}>
         <AuthInput
           text="이메일"
-          status={emailStatus}
           type="email"
           name="email"
           value={email}
           placeholder="이메일을 입력해주세요"
-          onChange={(val) => handleInputChange(val, 'email', setEmailStatus, setEmail)}
-          message={emailStatus === InputStatus.ERROR ? '이메일의 형식이 올바르지 않습니다.' : ''}
+          onChange={(val) => handleInputChange(val, 'email', 0, setEmail)}
+          message={hasError[0] ? '이메일의 형식이 올바르지 않습니다.' : ''}
           isCode
         />
         <AuthInput
           text="이메일 인증코드"
-          status={emailCodeStatus}
           type="string"
           name="emailCode"
           value={emailCode}
           placeholder="인증코드를 입력해주세요"
-          onChange={(val) => handleInputChange(val, 'emailCode', setEmailCodeStatus, setEmailCode)}
+          onChange={(val) => handleInputChange(val, 'emailCode', 1, setEmailCode)}
           onVerify={handleEmailVerification}
           isConfirm
         />
         <AuthInput
           text="비밀번호"
-          status={passwordStatus}
           type="password"
           name="password"
           value={password}
           placeholder="숫자, 영문자, 특수문자 조합으로 8자 이상 입력해주세요"
-          onChange={(val) => handleInputChange(val, 'password', setPasswordStatus, setPassword)}
-          message={
-            passwordStatus === InputStatus.ERROR
-              ? '비밀번호는 숫자, 영문자, 특수문자 조합으로 8자 이상이어야 합니다.'
-              : ''
-          }
+          onChange={(val) => handleInputChange(val, 'password', 2, setPassword)}
+          message={hasError[2] ? '비밀번호는 숫자, 영문자, 특수문자 조합으로 8자 이상이어야 합니다.' : ''}
           disabled={!isEmailVerified}
         />
         <AuthInput
           text="비밀번호 확인"
-          status={passwordConfirmStatus}
           type="password"
           name="passwordConfirm"
           value={passwordConfirm}
           placeholder="비밀번호를 다시 한번 입력해주세요"
-          onChange={(val) =>
-            handleInputChange(val, 'passwordConfirm', setPasswordConfirmStatus, setPasswordConfirm, password)
-          }
-          message={passwordConfirmStatus === InputStatus.ERROR ? '비밀번호가 일치하지 않습니다.' : ''}
+          onChange={(val) => handleInputChange(val, 'passwordConfirm', 3, setPasswordConfirm, password)}
+          message={hasError[3] ? '비밀번호가 일치하지 않습니다.' : ''}
           disabled={!isEmailVerified}
         />
         <div className={cx('buttonContainer')}>
