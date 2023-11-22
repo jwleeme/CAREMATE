e174701a053cfa3ac358d3fe16077a83fdc59f4b
chloe <imherejw@gmail.com>
2023-11-22 17:12:25 +0900

    Rename: 메시지함 구현 컴포넌트 연결

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { RecoilRoot } from 'recoil';
 import { Footer, Header, MaxWidth } from './components';
-
 import './styles/index.scss';
 import { Outlet } from 'react-router-dom';
+import MessageButton from 'components/common/message/MessageButton';
+
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <MaxWidth >
             <Outlet />
           </MaxWidth>
+          {/* 메시지함(채팅방) 버튼 컴포넌트 */}
+            <MessageButton/>
         </main>
         <MaxWidth>
           <Footer />
