1f1862371fe971926e551c964150dc1a670bebbb
현규 박 <wnsvmfh77@gmail.com>
2023-11-23 23:52:03 +0900

    feat: 서비스소개, 메인

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './Home.module.scss';
 import cs from 'classnames/bind';
+import InfantImage from '../../assets/images/Infant.png';
+import SeniorOneImage from '../../assets/images/SeniorOne.png';
+import DisabledImage from '../../assets/images/Disabled.png';
+import MainLogo from '../../assets/images/logo.png';
 const cx = cs.bind(styles);
 
 export default function Home() {
-    return (
-        <>
-            <h1>메인페이지 </h1>
-        </>
-    );
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('mainImage')}>
+        <img src={MainLogo} alt="메인로고" />
+      </div>
+      <div className={cx('cardContainer')}>
+        <div className={cx('card')}>
+          <span>아동</span>
+          <img src={InfantImage} alt="아동" />
+        </div>
+        <div className={cx('card')}>
+          <span>노인</span>
+          <img src={SeniorOneImage} alt="노인" />
+        </div>
+        <div className={cx('card')}>
+          <span>장애인</span>
+          <img src={DisabledImage} alt="장애인" />
+        </div>
+      </div>
+    </div>
+  );
 }
