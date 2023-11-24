77b635a6f47b2c3d6aff78e1da6a30590e2e08ff
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-11-24 21:53:27 +0900

    Remove: recoil-persist

--- src/recoil/storage.js
+++ src/recoil/storage.js
@@ -1,7 +1,4 @@
 import { atom } from 'recoil';
-import { recoilPersist } from 'recoil-persist';
-
-const { persistAtom } = recoilPersist();
 
 // 원하는 전역변수를 아래와 같이 선언해주시고 사용하시면 됩니다.
 // 원하는 위치에서 이렇게 사용하시면 됩니다.
@@ -16,5 +13,4 @@ const { persistAtom } = recoilPersist();
 export const isLoggedInState = atom({
   key: 'isLoggedInState',
   default: false,
-  effects_UNSTABLE: [persistAtom], // 영구저장
 });
