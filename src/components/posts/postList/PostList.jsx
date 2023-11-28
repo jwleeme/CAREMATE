e332388f795e8f83bcce9a22f162ed3c949fbdbf
Dawon Shin <shindw0314@gmail.com>
2023-11-29 00:13:26 +0900

    design: fix posts list css

--- src/components/posts/postList/PostList.jsx
+++ src/components/posts/postList/PostList.jsx
@@ -5,7 +5,7 @@ import styles from './PostList.module.scss';
 import cs from 'classnames/bind';
 const cx = cs.bind(styles);
 
-export default function PostList({ postsData, searchInput, currentPage }) {
+export default function PostList({ postsData, searchInput, currentPage, onPageChange }) {
   const postsList = postsData.posts;
   const [searchedPostsList, setSearchedPostsList] = useState([]);
 
@@ -18,24 +18,13 @@ export default function PostList({ postsData, searchInput, currentPage }) {
     }
   }, [searchInput, postsList]);
 
-
-  // const filteredPosts = searchInput
-  //     ? postsList.filter((card) => card.title && card.title.includes(searchInput))
-  //     : postsList;
-
-  //   const startIndex = currentPage * 6;
-  //   const endIndex = startIndex + 6;
-  //   const paginatedPosts = filteredPosts.slice(startIndex, endIndex);
-
-  //   setSearchedPostsList(paginatedPosts);
-  // }, [searchInput, postsList, currentPage]);
-
   return (
     <div className={cx('wrapper')}>
       {searchedPostsList.length > 0 ? (
         <div className={cx('card-list-container')}>
           {searchedPostsList.map((data, index) => (
             <Link to={`./${data._id}`} key={index}>
+              {console.log(`data ${index}:`, data)}
               <Card data={data} />
             </Link>
           ))}
--- src/hooks/getPostList.js
+++ src/hooks/getPostList.js
@@ -3,38 +3,17 @@ import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router';
 import { errorHandler } from 'lib';
 
-// const getPostList = async (pageNumber, careTarget, isLongTerm) => {
-//   const response = await axios.get(`/api/post?page=${pageNumber}&limit=6&careTarget=${careTarget}&isLongTerm=${isLongTerm}`, {
-//     params: {
-//       page: pageNumber,
-//       limit: 6,
-//       careTarget,
-//       isLongTerm,
-//     },
-//     withCredentials: true,
-//   });
-//   return response.data.data;
-// };
-
-// export function useGetPostList(pageNumber, careTarget, isLongTerm) {
-//   const postQuery = useQuery(['getPostList', pageNumber, careTarget, isLongTerm], () => getPostList(pageNumber, careTarget, isLongTerm), {
-//     cacheTime: 10 * 60 * 1000,
-//     staleTime: Infinity,
-//   });
-
-// }
-
-const getPostList = async (pageNumber) => {
-  const response = await axios.get(`/api/post?page=${pageNumber}&limit=6`, {
+const getPostList = async (pageNumber, careTarget) => {
+  const response = await axios.get(`/api/post?page=${pageNumber}&limit=6?careTarget=${careTarget}`, {
     withCredentials: true,
   });
   return response.data.data;
 };
 
-export function useGetPostList(pageNumber) {
+export function useGetPostList(pageNumber, careTarge) {
   const navigate = useNavigate();
 
-  return useQuery(['getPostList', pageNumber], () => getPostList(pageNumber), {
+  return useQuery(['getPostList', pageNumber, careTarge], () => getPostList(pageNumber, careTarge), {
     cacheTime: 10 * 60 * 1000,
     onError: (error) => {
       errorHandler(error, navigate);
