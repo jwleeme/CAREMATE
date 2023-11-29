26ffcb1cb66946929654691b8c4fddd80efcded0
Dawon Shin <shindw0314@gmail.com>
2023-11-29 16:24:23 +0000

    Merge branch 'feat/dawon' into dev

--- src/pages/posts/allPosts/AllPosts.jsx
+++ src/pages/posts/allPosts/AllPosts.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './AllPosts.module.scss';
 import cs from 'classnames/bind';
 import { Pagination, FilterCareTarget, SearchBar, PostList } from 'components';
 import { useGetPostList } from 'hooks';
-import { useLocation } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
 const cx = cs.bind(styles);
 
@@ -11,9 +10,23 @@ export default function AllPosts() {
   const [searchInput, setSearchInput] = useState('');
   const [currentPage, setCurrentPage] = useState(0);
   const [searchParams, setSearchParams] = useSearchParams();
-  const careTarget = searchParams.get('careTarget');
+  const [searchTarget, setSearchTarget] = useState('');
+  const [searchIsLongTerm, setSearchIsLongTerm] = useState('');
 
-  const { data: postsData, isLoading } = useGetPostList(currentPage + 1, careTarget);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchParams({ careTarget: searchParams.get('careTarget'), isLongTerm: searchParams.get('isLongTerm') });
+  };
+  const { data: postsData, isLoading } = useGetPostList(currentPage + 1, searchTarget, searchIsLongTerm);
+
+  console.log(postsData);
+  // let totalPage = 0;
+
+  // if (postsData) {
+  //   totalPage = postsData.totalCount;
+  // } else {
+  //   return <div className={cx('loading')}>로딩중...</div>;
+  // }
 
   const handleSearchChange = (text) => {
     setSearchInput(text);
@@ -23,15 +36,16 @@ export default function AllPosts() {
     <div className={cx('wrapper')}>
       <SearchBar className={cx('all-posts-style')} searchInput={searchInput} onSearchChange={handleSearchChange} />
       <div className={cx('recruit-container')}>
-        <FilterCareTarget postsData={postsData} />
+        <FilterCareTarget />
+
         {isLoading || !postsData ? (
           <div className={cx('loading')}>로딩중...</div>
         ) : (
-          <PostList postsData={postsData} searchInput={searchInput} pageNumber={currentPage + 1} />
+          <PostList postsData={postsData} searchInput={searchInput} target={searchTarget} />
         )}
-        <div className={cx('pagination-container')}>
-          <Pagination currPage={currentPage} onClickPage={setCurrentPage} pageCount={10} />
-        </div>
+        {/* <div className={cx('pagination-container')}>
+          <Pagination currPage={currentPage} onClickPage={setCurrentPage} pageCount={Math.ceil(totalPage / 6)} />
+        </div> */}
       </div>
     </div>
   );
