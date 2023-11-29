9e99dc5ac4cf12177f9d2f164c05dfd8c5a061a2
[SW6] 박제성 <richdad6208@gmail.com>
2023-11-29 00:54:31 +0000

    Fix: 전체게시글 조회 오류수정

--- src/hooks/getPostList.js
+++ src/hooks/getPostList.js
@@ -4,16 +4,16 @@ import { useNavigate } from 'react-router';
 import { errorHandler } from 'lib';
 
 const getPostList = async (pageNumber, careTarget) => {
-  const response = await axios.get(`/api/post?page=${pageNumber}&limit=6?careTarget=${careTarget}`, {
+  const response = await axios.get(`/api/post?page=${pageNumber}&limit=6&careTargets=${careTarget}`, {
     withCredentials: true,
   });
   return response.data.data;
 };
 
-export function useGetPostList(pageNumber, careTarge) {
+export function useGetPostList(pageNumber, careTarget) {
   const navigate = useNavigate();
 
-  return useQuery(['getPostList', pageNumber, careTarge], () => getPostList(pageNumber, careTarge), {
+  return useQuery(['getPostList'], () => getPostList(pageNumber, careTarget), {
     cacheTime: 10 * 60 * 1000,
     onError: (error) => {
       errorHandler(error, navigate);
