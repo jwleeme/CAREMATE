f776d4f14c2172ceffa05e59d85ed0186ecd4ea0
Dawon Shin <shindw0314@gmail.com>
2023-11-30 15:26:30 +0900

    feat: data mapping in message list

--- src/components/common/message/MessageList.jsx
+++ src/components/common/message/MessageList.jsx
@@ -10,66 +10,9 @@ const cx = cs.bind(styles);
 
 // 메시지함 리스트 컴포넌트 (채팅형식 UI - 레이어 팝업 형태)
 export default function MessageList(props) {
-  // mock data
-  //   const [chatList] = useState([
-  //   {
-  //     postId: 1,
-  //     username: '홍길동',
-  //     keyword: '노인',
-  //     postTitle: '병원 동행해주실 메이트분 구합니다.',
-  //     messagetext: '가지고 계신 지병이 있나요?',
-  //     lastdate: "2023-11-20",
-  //     lasttime: '오후 2:30'
-
-  //   },
-  //   {
-  //     postId: 2,
-  //     username: '김영희',
-  //     keyword: '아동',
-  //     postTitle: '등하원 시터 구합니다!',
-  //     messagetext: '아이가 몇시에 등원하나요?',
-  //     lastdate: "2023-11-15",
-  //     lasttime: '오전 7:00'
-
-  //   },
-  //   {
-  //     postId: 3,
-  //     username: '윤철수',
-  //     keyword: '장애인',
-  //     postTitle: '게시글 제목입니다',
-  //     messagetext: '안녕하세요! 돌봄메이트 윤철수입니다. 혹시 살고 계신 위치가 어디신가요?',
-  //     lastdate: "2023-11-01",
-  //     lasttime: '오후 2:30'
-
-  //     },
-  //     {
-  //       postId: 4,
-  //       username: '안미나',
-  //       keyword: '노인',
-  //       postTitle: '게시글 제목이에요',
-  //       messagetext: '할머니 연세가 어떻게되시나요?',
-  //       lastdate: "2023-10-19",
-  //       lasttime: '오후 8:00'
-
-  //     },
-  //     {
-  //       postId: 5,
-  //       username: '서미정',
-  //       keyword: '아동',
-  //       postTitle: '게시글 제목입니당',
-  //       messagetext: '혹시 시급 협의가 될까요?',
-  //       lastdate: "2023-10-07",
-  //       lasttime: '오전: 9:00'
-
-  //     }
-  // ]);
-
   const role = useRecoilValue(roleState);
 
-  // role==='user' ? ~~ : ~~~
-
   const [chatList, setChatList] = useState([]);
-  const [postlist, setPostList] = useState([]);
   const [currentPage, setCurrentPage] = useState([]);
 
   const { data: roomData } = useGetChatRooms(currentPage + 1);
@@ -77,14 +20,16 @@ export default function MessageList(props) {
   useEffect(() => {
     if (roomData) {
       const mapRoomsList = roomData.chats.map((room) => ({
-        postId: room.post._id,
+        chatId: room._id,
         postNumber: room.post.postNumber,
-        username: room.applicant.name,
-        keyword: room.post.careInformation.careTarget,
+        careUsername: room.applicant.name,
+        username: room.author.name,
+        careTarget: room.post.careInformation.careTarget,
         postTitle: room.post.title,
         messagetext: room.message.content,
         updateDate: room.message.updatedAt,
-        profileImage: room.applicant.profileUrl,
+        careUserProfileImage: room.applicant.profileUrl,
+        userProfileImage: room.author.profileUrl,
       }));
       setChatList(mapRoomsList);
     }
@@ -112,16 +57,21 @@ export default function MessageList(props) {
                 <li
                   className={cx('message-item')}
                   onClick={() => {
-                    props.chatInfoSelect(chatItem.postId);
+                    props.chatInfoSelect(chatItem.chatId);
                   }}
                 >
                   {/* 프로필사진, n이미지 영역 */}
                   <div className={cx('user-profilebox')}>
                     <img
                       className={cx('img-profile')}
-                      src={chatItem?.profileImage || ProfileImage}
+                      src={
+                        role === 'user'
+                          ? chatItem?.careUserProfileImage || ProfileImage
+                          : chatItem?.userProfileImage || ProfileImage
+                      }
                       alt="상대유저 프로필이미지"
                     />
+
                     <img className={cx('img-newmessage')} src={NewMessageImage} alt="새메시지이미지" />
                   </div>
 
@@ -132,8 +82,10 @@ export default function MessageList(props) {
                       {chatItem.postTitle}
                     </p>
 
-                    <span className={cx('username')}>{chatItem.username}</span>
-                    <span className={cx('keyword')}>{chatItem.keyword}</span>
+                    <span className={cx('username')}>
+                      {role === 'user' ? chatItem.careUsername : chatItem.username}
+                    </span>
+                    <span className={cx('careTarget')}>{chatItem.careTarget}</span>
                     <p className={cx('message-text')}>{chatItem.messagetext}</p>
                   </div>
 
