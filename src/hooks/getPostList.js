e42988f12cba2f9905f224596865290f7b963cb7
[SW6] 박제성 <richdad6208@gmail.com>
2023-11-30 08:05:33 +0000

    feature: 전체게시글 페이지네이션, 필터링 오류수정

--- src/hooks/getPostList.js
+++ src/hooks/getPostList.js
@@ -4,26 +4,39 @@ import { errorHandler } from 'lib';
 import { useRecoilValue } from 'recoil';
 import { isLoggedInState } from 'recoil/isLoggedInState';
 
-const getPostList = async (pageNumber, careTarget, term) => {
-  console.log(pageNumber, careTarget, term);
-  const response = await axios.get(
-    `/api/post?page=${pageNumber}&limit=6&careTargets=${careTarget}&isLongTerm=${term}`,
-    {
+const getPostList = async ({ showPage, careTarget, isLongTerm }) => {
+  if (typeof isLongTerm === 'string') {
+    const response = await axios.get(
+      `/api/post?page=${showPage}&limit=6&careTarget=${careTarget}&isLongTerm=${isLongTerm}`,
+      {
+        withCredentials: true,
+      }
+    );
+    return response.data.data;
+  } else if (!careTarget) {
+    const response = await axios.get(`/api/post?page=${showPage}&limit=6`, {
       withCredentials: true,
-    }
-  );
-  return response.data.data;
+    });
+    return response.data.data;
+  } else {
+    const response = await axios.get(`/api/post?page=${showPage}&limit=6&careTarget=${careTarget}`, {
+      withCredentials: true,
+    });
+    return response.data.data;
+  }
 };
 
-export function useGetPostList(pageNumber, careTarget, term) {
+export function useGetPostList({ showPage, careTarget, isLongTerm }) {
   const loginStatus = useRecoilValue(isLoggedInState);
-
-  return useQuery(['getPostList'], () => getPostList(pageNumber, careTarget, term), {
-    cacheTime: 10 * 60 * 1000,
-    onError: (error) => {
-      errorHandler(error);
-    },
-    retry: 0,
-    enabled: loginStatus !== 'LOADING',
-  });
+  return useQuery(
+    ['getPostList', showPage, careTarget, isLongTerm],
+    () => getPostList({ showPage, careTarget, isLongTerm }),
+    {
+      onError: (error) => {
+        errorHandler(error);
+      },
+      retry: 0,
+      enabled: loginStatus !== 'LOADING',
+    }
+  );
 }
